8f31d25952888bb5f931fc49fd47b7ae87b4c0a8
Nicolas Gnyra <nicolas@gnyra.com>
2021-09-12 02:00:41 -0400

    Switch to ejson for credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -8,18 +8,17 @@ end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
-  idp_metadata = idp_metadata_parser.parse_remote_to_hash(ENV['IDP_METADATA_URL'])
+  idp_metadata = idp_metadata_parser.parse_remote_to_hash(Rails.application.secrets.dig(:saml, :idp_metadata_url))
 
   provider :saml,
     idp_metadata.merge(
-      issuer: 'print.makerepo.com',
-      idp_entity_id: 'https://makerepo.com/saml/auth',
-      idp_sso_service_url: 'https://makerepo.com/saml/auth',
-      name_identifier_format: 'urn:oasis:names:tc:SAML:2.0:nameid-format:persistent',
+      issuer: Rails.application.secrets.dig(:saml, :issuer),
+      idp_entity_id: Rails.application.secrets.dig(:saml, :idp_entity_id),
+      idp_sso_service_url: Rails.application.secrets.dig(:saml, :idp_sso_service_url),
       attribute_statements: {
         name: %w(name),
-        email: %w(email_address),
-        nickname: %w(username),
+        email: %w(email_address email mail),
+        nickname: %w(nickname username),
       }
     )
 end
