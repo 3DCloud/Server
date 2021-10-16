1696ee592286c44b84fcb7ae3bed97678291ae37
Nicolas Gnyra <nicolas@gnyra.com>
2021-10-15 21:33:53 -0400

    Order by descending creation date by default

--- app/models/uploaded_file.rb
+++ app/models/uploaded_file.rb
@@ -4,4 +4,6 @@ class UploadedFile < ApplicationRecord
   has_one_attached :file
 
   delegate :filename, :content_type, :byte_size, :checksum, to: :file
+
+  default_scope { order(created_at: :desc) }
 end
